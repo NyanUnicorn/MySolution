1b57536776b277da5949cbd3ce1cc3e704b711fd
Phillip Redfern <philllip347@gmail.com>
2018-09-24 12:23:15 +0200

    petit entrainement

---
+++ MySolution.MyProject0/Model/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySolution.MyProject0.Model
+{
+
+    /// <summary>
+    ///     Classe de données qui représente une personne
+    /// </summary>
+    class Person // tout classe hérite de la classe "Object" (object == mot clé de la classe: minuscule pour mot clé)
+    {
+        #region Fields
+        /// <summary>
+        ///     prénom de la personne
+        /// </summary>
+        private string firstName;
+        private string lastName;
+        private DateTime birthDdate;
+
+        #endregion
+
+        #region Properties
+        /// <summary>
+        ///     Obtien ou définit le prénom de la personne
+        /// </summary>
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public DateTime BirthDdate { get => birthDdate; set => birthDdate = value; }
+
+        #endregion
+
+
+        #region Constructors
+        public Person()
+        {
+
+        }
+        
+        #endregion
+
+
+        #region Methods
+
+        #endregion
+    }
+}
--- MySolution.MyProject0/Program.cs
+++ MySolution.MyProject0/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
+﻿using MySolution.MyProject0.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks; // utilisation de namespace (grisé si pas encor utilisé)
 
 namespace MySolution.MyProject0
 {
-    class Program
+    class Program // class est une schéma pour un/des objet(s)
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // méthode -- signature(ce qu'elle retourne / ses parametres)
         {
+            Person maPersonne = new Person();
+            maPersonne.FirstName = "James";
+            maPersonne.LastName = "Bourne";
+
+            string message;
+            message = "Bonjour " + maPersonne.FirstName + " " + maPersonne.LastName;
+            message = string.Format(" Bonjour {0} {1}"
+                , maPersonne.FirstName
+                , maPersonne.LastName);
+            message = $"Bonjour {maPersonne.FirstName} {maPersonne.LastName}";
+            Console.WriteLine(message);
+            Console.ReadKey();
         }
     }
 }
